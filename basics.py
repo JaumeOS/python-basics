d29ccba0b88eaef56016e67afee69dbf08c8fcd2
JaumeOS <77398566+JaumeOS@users.noreply.github.com>
2022-12-23 03:52:34 +0100

    basics.py and lists.py added

---
+++ basics.py
@@ -0,0 +1,18 @@
+# las variables asumen un tipo de dato cuando se le asigna un valor
+entero = 1
+decimal = 1.05
+print('Número entero: ', entero, )
+print('Número decimal: ', decimal)
+
+x = 10
+y = 5
+resto = x % y
+# el resto de la división 10/5 és igual a 0, ya que la división és exacta
+print('Resto de la división ', x, '/', y, ' = ', resto)
+
+print([1,2,3] * 3)
+
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
